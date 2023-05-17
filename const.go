80839a5cf3e0afaa3558d81c6295e8e71fc4c63a
Abdul Salam <110958791+abdulsalamtokped@users.noreply.github.com>
2023-05-17 15:04:21 +0700

    REF: chain func

--- const.go
+++ const.go
@@ -2,19 +2,19 @@ package main
 
 const (
 	ruleBag = `
-		rule CheckModule "Check package type v1" {
+		rule CheckModule "Check package type v1" salience 1 {
 			when
 				Package.Module == Module
 			then
 				CheckType();
 		}
-		rule CheckType "Check Package" {
+		rule CheckType "Check Package" salience 2 {
 			when
 				Package.Type == Type
 			then
 				CheckValidation();
 		}
-		rule CheckValidation "Check Validation" {
+		rule CheckValidation "Check Validation" salience 3 {
 			when
 				Package.RunValidationWarehouseCheck() == true && Package.RunValidationShipper() == true
 			then
